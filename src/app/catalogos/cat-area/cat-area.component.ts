0a618289e423f0a02627c31de42824d0725b0e9d
miguel <mmeraz@netasystems.net>
2018-11-29 13:54:44 -0600

    otro commit

--- src/app/catalogos/cat-area/cat-area.component.ts
+++ src/app/catalogos/cat-area/cat-area.component.ts
@@ -1,10 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Catarea } from 'src/app/model/catarea.model';
 import { AreaService } from 'src/app/service/cat.area.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {Router, ActivatedRoute} from '@angular/router';
-import { Location } from '@angular/common';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute} from '@angular/router';
 import swal from 'sweetalert2';
+
+
+class DataTablesResponse {
+  data: any[];
+  draw: number;
+  recordsFiltered: number;
+  recordsTotal: number;
+}
+
+class Person {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
+
 
 @Component({
   selector: 'app-cat-area',
@@ -14,27 +29,42 @@ import swal from 'sweetalert2';
 export class CatAreaComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
   arrayAreas: Catarea[];
-  selectArea: Catarea;
+  persons: Person[];
 
   constructor( private areaService: AreaService,
     private fb: FormBuilder, private bs: AreaService,
-    private activatedRoute: ActivatedRoute) {
+    private activatedRoute: ActivatedRoute,
+    private http: HttpClient) {
     }
 
-  ngOnInit() {
-    this.areaService.getAreas().subscribe(
-      (data: Catarea[]) => this.arrayAreas = data
-    );
+  ngOnInit(): void {
+    const that = this;
+
+    // this.areaService.getAreas().subscribe(
+    //   (data: Catarea[]) => this.arrayAreas = data
+    // );
 
     this.dtOptions = {
-      ajax: 'data/data.json',
-      columns: [{
-        title: 'Id',
-        data: 'arnIdarean'
-      }, {
-        title: 'Área',
-        data: 'arnTipo'
-      }]
+      pagingType: 'full_numbers',
+      pageLength: 2,
+      serverSide: true,
+      processing: true,
+      ajax: (dataTablesParameters: any, callback) => {
+        that.http
+          .post<DataTablesResponse>(
+            'http://localhost:8085/api/v1/areaNegocio/fetch',
+            dataTablesParameters, {}
+          ).subscribe(resp => {
+            that.arrayAreas = resp.data;
+
+            callback({
+              recordsTotal: resp.recordsTotal,
+              recordsFiltered: resp.recordsFiltered,
+              data: []
+            });
+          });
+      },
+      columns: [{ data: 'arnIdarean' }, { data: 'arnTipo' }]
     };
   }
 
