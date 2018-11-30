4addfb0711d9ef07dd88f91841adcf02cde23dc9
Yizuzleyton <42563566+Yizuzleyton@users.noreply.github.com>
2018-11-30 16:44:00 -0600

    .

--- src/app/catalogos/cat-contactcliente/cat-contactcliente.component.ts
+++ src/app/catalogos/cat-contactcliente/cat-contactcliente.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Catcontactcliente} from 'src/app/model/catcontactcliente.model';
+import { ContactClienteService} from 'src/app/service/cat.contactcliente.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import { Catcliente } from 'src/app/model/catcliente.model';
+
 
 @Component({
   selector: 'app-cat-contactcliente',
   templateUrl: './cat-contactcliente.component.html',
-  styles: []
+  providers: [ContactClienteService]
 })
 export class CatContactclienteComponent implements OnInit {
+  contacto: Catcliente[];
+  cliente: Catcliente = null;
+  arrayContactCliente: Catcontactcliente[] = [];
 
-  constructor() { }
+  constructor(private contaclienteService: ContactClienteService,
+    private fb: FormBuilder, private bs: ContactClienteService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.contaclienteService.getContactcliente().subscribe
+      (contacto => this.contacto = contacto
+    );
   }
 
 }
+
