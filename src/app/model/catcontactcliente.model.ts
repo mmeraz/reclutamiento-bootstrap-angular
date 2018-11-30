a931acb2cc181a6a463cb5d72442deb56a4ab627
Yizuzleyton <42563566+Yizuzleyton@users.noreply.github.com>
2018-11-30 16:47:37 -0600

    Update catcontactcliente.model.ts
    .

--- src/app/model/catcontactcliente.model.ts
+++ src/app/model/catcontactcliente.model.ts
@@ -1,4 +1,8 @@
 export interface Catcontactcliente {
-    
- 
+  clcIdconcli: number;
+  clcNombre: string;
+  clcTelefono: string;
+  clcEmail: string;
+  clcTipo: string;
+  clcExtension: string;
 }
