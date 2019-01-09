106035bd70abe84ff552658e00638e7c208b4eca
miguel <mmeraz@netasystems.net>
2019-01-09 14:09:46 -0600

    candidatos validados

--- src/app/pages/usuarios/ti/components/candi-validados-ti.component.ts
+++ src/app/pages/usuarios/ti/components/candi-validados-ti.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { Catseguicandidato } from '../../../../model/catseguicandidato.model';
+import { Subject } from 'rxjs';
+import { SeguimientoCandService } from '../../../../service/seguimientocandidato.service';
+import { AuthService } from '../../../../service/auth.service';
 
 @Component({
   selector: 'app-candi-validados-ti',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandiValidadosTiComponent implements OnInit {
 
-  constructor() { }
+
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
+  dtOptions: DataTables.Settings = {};
+  arrayCandidatos: Catseguicandidato[];
+  allSeguimiento: any = [];
+  dtTrigger: Subject<any> = new Subject();
+  constructor(private serviceSecCandidato: SeguimientoCandService, protected authservice: AuthService) { }
 
   ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+    };
+    this.serviceSecCandidato. getSecSeguimientoPendienteEntrevista().subscribe(result => {
+      this.allSeguimiento = result;
+      this.dtTrigger.next();
+    });
+  }
+
+  getListSize(): number {
+    return 0;
   }
 
 }
