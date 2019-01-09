7a2cc1b76748b4308c5ed0a2ca634ddcb74642dc
miguel <mmeraz@netasystems.net>
2019-01-09 08:24:53 -0600

    cambios seguimiento candidato

--- src/app/pages/usuarios/backoffice/componets/requipe-backoffice.component.ts
+++ src/app/pages/usuarios/backoffice/componets/requipe-backoffice.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { CatSeguimientoSol } from '../../../../model/catsegimientosolicitud.model';
+import { Subject } from 'rxjs';
+import { SeguimientoSolService } from '../../../../service/seguimientosol.service';
+import { AuthService } from '../../../../service/auth.service';
 
 @Component({
   selector: 'app-requipe-backoffice',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequipeBackofficeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
+  dtOptions: DataTables.Settings = {};
+  arrayAreas: CatSeguimientoSol[];
+  allSeguimiento: any = [];
+  dtTrigger: Subject<any> = new Subject();
+  constructor(private serviceSgsSolicitud: SeguimientoSolService, protected authservice: AuthService) { }
 
   ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+    };
+    this.serviceSgsSolicitud.getSgsSeguimietoAceptadaXPerdida().subscribe(result => {
+      this.allSeguimiento = result;
+      this.dtTrigger.next();
+    });
+  }
+
+  getListSize(): number {
+    return 0;
   }
 
 }
