f59b7b422b1f85cacb298266311d2e496d190062
miguel <mmeraz@netasystems.net>
2019-02-19 09:32:46 -0600

    cambios backoffice

--- src/app/pages/usuarios/backoffice/candidatos/pendienteDoc/cand-pend-doc/cand-pend-doc.component.ts
+++ src/app/pages/usuarios/backoffice/candidatos/pendienteDoc/cand-pend-doc/cand-pend-doc.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { Catseguicandidato } from '../../../../../../model/catseguicandidato.model';
+import { Catentrevista } from '../../../../../../model/catentrevista.model';
+import { Subject } from 'rxjs';
+import { SeguimientoCandService } from '../../../../../../service/seguimientocandidato.service';
+import { AuthService } from '../../../../../../service/auth.service';
+import { EntrevistaService } from '../../../../../../service/cat.entrevista.service';
 
 @Component({
   selector: 'app-cand-pend-doc',
@@ -7,9 +14,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandPendDocComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
+  dtOptions: DataTables.Settings = {};
+  arrayCandidatos: Catseguicandidato[];
+  arrayEntrevista: Catentrevista[];
+  allSeguimiento: any = [];
+  dtTrigger: Subject<any> = new Subject();
+
+  constructor(private serviceSecCandidato: SeguimientoCandService, protected authservice: AuthService,
+              private serviceEntrevista: EntrevistaService) { }
 
   ngOnInit() {
-  }
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+    };
+    this.serviceEntrevista.getSeguimientoEntrevista().subscribe(result => {
+      this.allSeguimiento = result;
+      this.dtTrigger.next();
+    });
+}
 
 }
